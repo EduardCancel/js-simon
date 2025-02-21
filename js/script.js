7086bb10f0e824d0875e7f7b90e83ba0a1ec85ff
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-21 03:39:37 +0100

    add  random number generator

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ const formControlEl = document.getElementsByClassName('.form-control')
 const btnEl = document.getElementsByClassName('btn')
 const messageEl= document.getElementById('message')
 console.log(countdownEl,instructionsEl, numberListEl,answerFormEl, inputGroupEl, formControlEl, btnEl, messageEl  );
+let correctNuber = 0;
+
+// Generate five random numbers
+let randomNumbArr = []
+for(let i = 0; i < 5; i++){
+    randomNumbArr.push(Math.floor(Math.random() * 50) +1)
+}
+console.log(randomNumbArr);
+
+// Show the number in console with a space
+
+
+// Functions
+
+// function
