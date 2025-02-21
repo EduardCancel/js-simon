4e4ec83970f1261c81d1ac9c1ecccc7efd0445a8
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-21 02:44:10 +0100

    add const EL

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//  Element functions
+
+const countdownEl = document.getElementById('countdown')
+const instructionsEl = document.getElementById('instructions')
+const numberListEl = document.getElementById('numbers-list')
+const answerFormEl = document.getElementById('answers-form')
+const inputGroupEl = document.getElementById('input-group')
+const formControlEl = document.getElementsByClassName('.form-control')
+const btnEl = document.getElementsByClassName('btn')
+const messageEl= document.getElementById('message')
+console.log(countdownEl,instructionsEl, numberListEl,answerFormEl, inputGroupEl, formControlEl, btnEl, messageEl  );
