da5107f664c770239df8cb04b81638f773060f68
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-23 12:37:35 +0200

    first reset file js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,10 @@
-// Element functions
+// Element Functions
 const countdownEl = document.getElementById('countdown');
 const instructionsEl = document.getElementById('instructions');
 const numberListEl = document.getElementById('numbers-list');
 const answerFormEl = document.getElementById('answers-form');
 const inputGroupEl = document.getElementById('input-group');
-const formControlEl = document.getElementsByClassName('form-control'); // Fixed selector
-const btnEl = document.getElementsByClassName('btn');
+const btnEl = document.querySelector('.btn'); // Seleziona il primo bottone con classe "btn"
 const messageEl = document.getElementById('message');
 
-console.log(countdownEl, instructionsEl, numberListEl, answerFormEl, inputGroupEl, formControlEl, btnEl, messageEl);
-
-let numbGenerate = [];
-let listItems = '';
-let timer = 20;
-
-// Generate five random numbers
-function generateRandomNumber() {
-    numbGenerate = [];
-    listItems = ''; // Reset listItems before each generation
-
-    for (let i = 0; i < 5; i++) {
-        const randomNumber = Math.round(Math.random() * 50) + 1;
-        numbGenerate.push(randomNumber);
-        listItems += `<li>${randomNumber}</li>`;
-    }
-
-    console.log(numbGenerate); // Logs the generated numbers correctly
-    numberListEl.innerHTML = `<ul>${listItems}</ul>`;
-
-    // Add countdown
-    const intervalID = setInterval(function () {
-        timer--;
-        countdownEl.innerText = timer;
-    
-        if (timer === 0) {
-            clearInterval(intervalID);
-            numberListEl.classList.add("d-none");
-            answerFormEl.classList.remove("d-none");
-        }
-    }, 1000);
-    
-}
-
-// Call the function to generate numbers
-generateRandomNumber();
-
-
-
-
-
+console.log(countdownEl, instructionsEl, numberListEl, answerFormEl, inputGroupEl, btnEl, messageEl);
